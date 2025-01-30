c6dd3396e59fc37dae300f4e39509b86a2216e44
mcuerod <mcuerod1910@g.educaand.es>
2025-01-30 14:53:41 +0100

    test

---
+++ calculadora.test.js
@@ -0,0 +1,9 @@
+const { suma, resta } = require('./calculadora');
+
+test('Suma 2 + 3 y devuelve 5', () => {
+    expect(suma(2, 3)).toBe(5);
+});
+
+test('Resta 5 - 3 y devuelve 2', () => {
+    expect(resta(5, 3)).toBe(2);
+});
